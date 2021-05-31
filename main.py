812ca5481e5f73e54e188c932a10764b4f6225de
Abhinav Kumar Choudhary <75004567+Brodevil@users.noreply.github.com>
2021-05-31 20:02:51 +0530

    Intial commit

--- main.py
+++ main.py
@@ -12,25 +12,18 @@ def speak(str):
 
 
 def news_api( apikey, url="https://newsapi.org/v2/top-headlines?sources=the-times-of-india&apikey=96926ea85ee242508e5527e0891be103"):
-    
-    if url == "https://newsapi.org/v2/top-headlines?sources=the-times-of-india&apikey=96926ea85ee242508e5527e0891be103":
-        response = requests.get("https://newsapi.org/v2/top-headlines?"
-                                "sources=the-times-of-india&"
-                                f"apiKey={apikey}")
-    else:
-        response = requests.get(url)
-
+    response = requests.get(url)
     json_data = json.loads(response.text)
-    return json_data
+    return json_data['articles']
 
 
 if __name__ == "__main__":
     top_ten = news_api()    # we have the acces to put the URL and our API key, Default link is also avalable 
-    arts = top_ten['articles']
-    for articles in arts:
+    for number, articles in enumarate(top_ten):
+        speak("Moving towards "+ "a " if number== 0 else "another " + "fresh news")
         print(f"Title: {articles['title']}. \nDiscription: {articles['description']}. Actually: {articles['content']}\nFor more info... Go to ==>>> {articles['url']}\n")
         speak(f"Title; {articles['title']}. \nDiscription; {articles['description']}. Actually; {articles['content']}\n")
-        speak("Moving On next news!")
+        
     speak("Thank you for listening")
               
  # how does the program is if you think you can best feel free to pull requests
